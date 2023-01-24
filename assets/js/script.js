f06f7f1b428ae12c9d65af584a18a5ce6b3ba0d0
shaizai1 <117948803+shaizai1@users.noreply.github.com>
2023-01-24 14:04:38 +0000

    added timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,7 @@
 var now = moment();
-$("#currentDay").text(now.format("dddd, MMMM do"));
+$("#currentDay").text(now.format("dddd, MMMM Do"));
+
+
+let timeBlockEl = $("#time-block");
+
+timeBlockEl.addClass("present");
